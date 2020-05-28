a23f4e2cc14599b7c86fa6db3f7a954b56db7bd6
Leonid Boginski <boginski.leonid@gmail.com>
2020-05-28 23:47:29 +0300

    create Point.cs

---
+++ Easy Grid/Point.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Easy_Grid
+{
+    public class Point
+    {
+        public double lat;
+        public double lon;
+
+        public Point(double lat, double lon)
+        {
+            this.lat = lat;
+            this.lon = lon;
+        }
+
+        public Point()
+        {
+            
+        }
+    }
+}
