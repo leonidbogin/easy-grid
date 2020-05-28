d647a636da3fb8bd757837e656efa3d1fb97eb5c
Leonid Boginski <boginski.leonid@gmail.com>
2020-05-28 23:47:17 +0300

    update CONST.cs

--- Easy Grid/Const.cs
+++ Easy Grid/Const.cs
@@ -8,10 +8,26 @@ namespace Easy_Grid
 {
     public static class CONST
     {
-        public static string POINTS_FILE = "LastSelection.hlg";
-        public static string POINT_LAT_1 = "PointLat_1=";
-        public static string POINT_LON_1 = "PointLon_1=";
-        public static string POINT_LAT_3 = "PointLat_3=";
-        public static string POINT_LON_3 = "PointLon_3=";
+        public static class LastSelection
+        {
+            public static string FileName = "LastSelection.hlg";
+            public static string Zoom = "Zoom=";
+            public static string Lat_1 = "PointLat_1=";
+            public static string Lon_1 = "PointLon_1=";
+            public static string Lat_2 = "PointLat_2=";
+            public static string Lon_2 = "PointLon_2=";
+            public static string Lat_3 = "PointLat_3=";
+            public static string Lon_3 = "PointLon_3=";
+            public static string Lat_4 = "PointLat_4=";
+            public static string Lon_4 = "PointLon_4=";
+            public static string Lat_5 = "PointLat_5=";
+            public static string Lon_5 = "PointLon_5=";
+        }
+        
+        public static class Error
+        {
+            public static string LastSelection = "Возможно проблема в заключается в том,\nчто программа не расположена в директории SAS Planet.\nЕсли так, закройте программу и переместите её в папку с программой SAS Planet.";
+            public static string LastSelectionHeader = "Ошибка копирования из файла LastSelection.hlg";
+        }
     }
 }
