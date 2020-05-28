2f582214508aff9dd31d1b1eb2b4e03739c88982
Leonid Boginski <boginski.leonid@gmail.com>
2020-05-28 21:49:43 +0300

    create CONST.cs

---
+++ Easy Grid/Const.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Easy_Grid
+{
+    public static class CONST
+    {
+        public static string POINTS_FILE = "LastSelection.hlg";
+        public static string POINT_LAT_1 = "PointLat_1=";
+        public static string POINT_LON_1 = "PointLon_1=";
+        public static string POINT_LAT_3 = "PointLat_3=";
+        public static string POINT_LON_3 = "PointLon_3=";
+    }
+}
