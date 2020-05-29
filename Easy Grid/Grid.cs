3abfe3b7091d381537834a8e624b39e5c4c7ad09
Leonid Boginski <boginski.leonid@gmail.com>
2020-05-29 09:32:56 +0300

    create Grid.cs

---
+++ Easy Grid/Grid.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Easy_Grid
+{
+    public class Grid
+    {
+        public Point[,] points;
+        public List<Point> line;
+        public int stepX = 0;
+        public int stepY = 0;
+
+        public Grid(Point[,] points, int stepX, int stepY)
+        {
+            this.points = points;
+            this.stepX = stepX;
+            this.stepY = stepY;
+        }
+    }
+}
